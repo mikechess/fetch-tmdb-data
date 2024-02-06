ae99d162822d7c7ab531d96d53cf7a730e37ab5e
mikechess <mikecook450@gmail.com>
2024-02-06 13:30:57 -0500

    01.Implementation Begins And Fetching Movies Data With Axios

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [movies, setMovies] = useState();
+
+  const apikey = import.meta.env.VITE_API_KEY;
+  const popular = "https://api.themoviedb.org/3/movie/popular";
+
+  useEffect(() => {
+    fetchData();
+  }, [])
+
+  const fetchData = () => {
+    axios.get(`${popular}?api_key=${apikey}`).then((response) => {
+      const results = response.data.results;
+      console.log(results);
+    })
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Hello World!</h1>
     </>
   )
 }
